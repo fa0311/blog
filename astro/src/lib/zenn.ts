eefeb49bfe6d8372439edfdbefc9fcfe397a6ec0
ふぁ <yuki@yuki0311.com>
2025-01-16 17:16:53 +0900

    feat
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- astro/src/lib/zenn.ts
+++ astro/src/lib/zenn.ts
@@ -1,34 +1,116 @@
-import type { MarkdownInstance } from "astro";
+import type { MarkdownHeading, MarkdownInstance } from "astro";
+import Path from "node:path";
 import "zenn-content-css";
-import { type Article } from "zenn-model";
 import markdownToHtml from "./markdownToHtml";
 
 import * as cheerio from "cheerio";
 import "zenn-content-css";
+import { getLatestCommitTime } from "./git";
 
-export type ZennArticle = MarkdownInstance<Article>;
-export const getArticleList = () => {
-  const slugs = import.meta.glob<ZennArticle>("../../../articles/*.md");
-  return Promise.all(Object.values(slugs).map((article) => article()));
+type Frontmatter = {
+  title: string;
+  emoji: string;
+  type: "tech" | "idea";
+  topics: string[];
+  published: boolean;
+  astro?: boolean;
 };
 
-export const getRelatedArticles = (articles: ZennArticle[], current: ZennArticle) => {
+type ArticleListRawResponse = {
+  file: string;
+  contents: MarkdownInstance<Frontmatter>;
+};
+
+const getArticleListRaw = async (): Promise<ArticleListRawResponse[]> => {
+  const slugs = import.meta.glob<MarkdownInstance<Frontmatter>>("../../../articles/*.md");
+  const article = Object.entries(slugs).map(async ([file, contents]) => {
+    return { file: file, contents: await contents() };
+  });
+  return await Promise.all(article);
+};
+
+type ArticleListResponse = {
+  file: string;
+  contents: MarkdownInstance<Frontmatter>;
+  lastCommit?: Date;
+};
+
+const getArticleList = async (): Promise<ArticleListResponse[]> => {
+  const raw = await getArticleListRaw();
+  const enable = raw.filter((article) => article.contents.frontmatter.astro === true);
+  const article = enable.map(async (article) => {
+    const lastCommit = await getLatestCommitTime(Path.relative("../../", article.file));
+    return { ...article, lastCommit };
+  });
+  return await Promise.all(article);
+};
+
+export const getArticleData = async () => {
+  const data = await getArticleList();
+  return getArticleDataRecursive(data.sort((a, b) => timeSort(a.lastCommit, b.lastCommit)));
+};
+
+const getArticleDataRecursive = (articles: ArticleListResponse[]) => {
+  return articles.map((article) => {
+    return {
+      slug: Path.basename(article.file, ".md"),
+      lastCommit: article.lastCommit,
+      frontmatter: article.contents.frontmatter,
+      getContent: () => markdownToHtmlNormalized(article.contents.rawContent()),
+      getRelatedArticles: () => getArticleDataRecursive(getRelatedArticles(articles, article)),
+      getNavigation: () => headingsNormalize(article.contents.getHeadings()),
+    };
+  });
+};
+
+const getRelatedArticles = (articles: ArticleListResponse[], current: ArticleListResponse) => {
   const relatedArticles = articles
     .filter((article) => article.file !== current.file)
     .map((article) => {
-      const tagA = article.frontmatter.tags ?? [];
-      const tagB = current.frontmatter.tags ?? [];
-      const similarity = calculateSimilarity(tagA, tagB);
-      return { ...article, similarity, random: Math.random() };
+      const a = article.contents.frontmatter.topics;
+      const b = current.contents.frontmatter.topics;
+      const similarity = calculateSimilarity(a, b);
+      return { ...article, similarity };
     })
     .sort((a, b) => {
       if (a.similarity !== b.similarity) {
         return b.similarity - a.similarity;
       } else {
-        return b.random - a.random;
+        return timeSort(a.lastCommit, b.lastCommit);
       }
     });
   return relatedArticles;
+};
+
+const timeSort = (a: Date | undefined, b: Date | undefined) => {
+  if (a == null && b == null) {
+    return 0;
+  } else if (a == null) {
+    return -1;
+  } else if (b == null) {
+    return 1;
+  } else {
+    return b.getTime() - a.getTime();
+  }
+};
+
+const headingsNormalize = (headings: MarkdownHeading[]) => {
+  if (headings.length === 0) {
+    return [];
+  } else {
+    const [top, ...rest] = headings;
+    const minDepth = Math.min(...rest.map((heading) => heading.depth));
+    const restLevel = (() => {
+      if (top.depth == minDepth) {
+        return rest.map((heading) => ({ ...heading, level: heading.depth + 1 }));
+      } else if (top.depth < minDepth) {
+        return rest.map((heading) => ({ ...heading, level: heading.depth }));
+      } else {
+        throw new Error("Invalid heading depth");
+      }
+    })();
+    return [{ ...top, level: 1 }, ...restLevel];
+  }
 };
 
 const calculateSimilarity = (a: string[], b: string[]) => {
@@ -55,6 +137,11 @@ export const markdownToHtmlNormalized = (raw: string) => {
   //     $(el).replaceWith(`<h${i + 1}>${$(el).html()}</h${i + 1}>`);
   //   });
   // });
+
+  $(`.header-anchor-link`).remove();
+  $(`iframe`).each((i, el) => {
+    $(el).attr("title", "embed");
+  });
   return {
     contents: () => $("body").html()!,
     description: () => $("p").first().text(),
