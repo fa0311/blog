539ff952f658a0835610685eb3d19527c54474eb
ふぁ <yuki@yuki0311.com>
2025-02-04 01:56:28 +0900

    add webp converter

--- astro/src/pages/assets/images/[...slug].ts
+++ astro/src/pages/assets/images/[...slug].ts
@@ -4,13 +4,7 @@ import imageminWebp from "imagemin-webp";
 import { promises as fs } from "node:fs";
 import Path, { sep } from "node:path";
 import { cache, getCached } from "../../../lib/cache";
-
-const pathSplit = (path: string) => {
-  const ext = Path.extname(path);
-  const name = Path.basename(path);
-  const dir = Path.dirname(path);
-  return [dir === "." ? "" : dir, name.slice(0, -ext.length), ext];
-};
+import { convertExt, pathSplit } from "../../../lib/path";
 
 export const GET: APIRoute = async ({ params, props }) => {
   const path = new URL(`../../../../../images/${params.slug}`, import.meta.url);
@@ -46,11 +40,6 @@ export const getStaticPaths = () => {
   const slugs = import.meta.glob("../../../../../images/**/*.{png,jpg,jpeg,webp,gif}");
   return Object.entries(slugs).map(([file]) => {
     const slug = Path.relative("../../../../../images", file).replaceAll(sep, "/");
-    const [dir, name, ext] = pathSplit(slug);
-    if ([".png", ".jpg", ".jpeg"].includes(ext.toLowerCase())) {
-      return { params: { slug: `${dir}/${name}.webp` } };
-    } else {
-      return { params: { slug: `${dir}/${name}${ext}` } };
-    }
+    return { params: { slug: convertExt(slug) } };
   });
 };
